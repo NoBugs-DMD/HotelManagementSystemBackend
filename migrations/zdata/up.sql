a15b49c6e0baf04963b2c602fc41f8a002fcb1b2
Mike Lubinets <lubinetsm@yandex.ru>
2016-11-17 20:15:09 +0300

    Some comments

--- migrations/zdata/up.sql
+++ migrations/zdata/up.sql
@@ -1,3 +1,4 @@
+-- Initial users setup
 INSERT INTO Person(Login, Name, Email, PassHash) VALUES('owner', 'Owner', 'owner@dmd.ru', '0');
 INSERT INTO Person(Login, Name, Email, PassHash) VALUES('manager', 'Manager', 'manager@dmd.ru', '0');
 INSERT INTO Person(Login, Name, Email, PassHash) VALUES('receptionist', 'Receptionist', 'receptionist@dmd.ru', '0');
@@ -7,3 +8,5 @@ INSERT INTO Owner(PersonID) VALUES ((SELECT Person.ID FROM Person WHERE Person.L
 INSERT INTO Manager(PersonID) VALUES ((SELECT Person.ID FROM Person WHERE Person.Login = 'manager'));
 INSERT INTO Receptionist(PersonID) VALUES ((SELECT Person.ID FROM Person WHERE Person.Login = 'receptionist'));
 INSERT INTO Cleaner(PersonID) VALUES ((SELECT Person.ID FROM Person WHERE Person.Login = 'cleaner'));
+
+-- The other data is populated with application
